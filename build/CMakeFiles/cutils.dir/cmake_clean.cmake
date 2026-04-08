8d07bf2aa3d340d0b8ed8cecae215c4d422b7cd0
DameChocolateYa <samujf009@gmail.com>
2026-04-08 23:15:42 +0200

    Added more functions to CString and added an early CVector

---
+++ build/CMakeFiles/cutils.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cutils.dir/link.d"
+  "CMakeFiles/cutils.dir/src/cstring.c.o"
+  "CMakeFiles/cutils.dir/src/cstring.c.o.d"
+  "libcutils.pdb"
+  "libcutils.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cutils.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
